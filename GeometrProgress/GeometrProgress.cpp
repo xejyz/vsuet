20ef06b78863b1247472aee0c3ded282fe66012e
xejyz <kendalzloy@gmail.com>
2024-04-15 21:25:18 +0300

    GeometrProgress

---
+++ GeometrProgress/GeometrProgress.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int GeometrProgress(int a,int r, int n) {
+	int s = a * (pow(r, n)) / (r - 1);
+	cout << "Сумма геометрической прогрессии" << s;
+	return s;
+}
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int a, r, n, s;
+	cout << "Введите первый член: ";
+	cin >> a;
+	cout << "Введите знаменатель прогрессии: ";
+	cin >> r;
+	cout << "Введите кол-во членов прогрессии: ";
+	cin >> n;
+	GeometrProgress(a, r, n);
+}
+
+
