31b654dc2e4b3e3726f46ad323a6983b69167e55
Дима Чурсанов <kendalzloy@gmail.com>
2024-04-16 13:05:02 +0300

    Создание прямоугольника и нахождение площади и периметра его

---
+++ Rectangle/Rectangle.cpp
@@ -0,0 +1,42 @@
+﻿#include <Windows.h>
+#include <iostream>
+
+using namespace std;
+
+class RectangleSimple {
+public:
+
+    RectangleSimple(int width, int height) {
+        
+    }
+    int Perimetr(int width, int height) {
+        int P = (width + height) + (width + height);
+        cout << "Периметр прямоугольника " << P;
+        return P;
+    }
+    int Square(int width, int height) {
+        int S = width * height;
+        cout << "Площадь прямоугольника " << S;
+        return S;
+    }
+};
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int width, height;
+
+    cout << "Введите высоту прямоугольника "; 
+    cin >> height;
+    cout << "Введите ширину прямоугольника "; 
+    cin >> width;
+
+    RectangleSimple rectangle(width, height);
+    rectangle.Perimetr(width,height);
+    cout << "\n";
+    rectangle.Square(width, height);
+    
+    
+}
+
